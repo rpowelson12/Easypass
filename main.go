46ceeca3973b99ed8dc9bcfd1a44cd73595ec53a
rpowelson12 <randy.powelson@gmail.com>
2025-06-05 20:56:26 -0700

    users done I think

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 	}
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
+	cmds.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
